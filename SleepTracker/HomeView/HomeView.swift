82a746beebde17d86edaca667023b7d3e24d1cdb
Alexander6Shevtsov <axrsvr@gmail.com>
2025-02-19 16:38:50 +0300

    Add Circle

--- SleepTracker/HomeView/HomeView.swift
+++ SleepTracker/HomeView/HomeView.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct HomeView: View {
     @StateObject private var homeViewModel = HomeViewModel()
     var body: some View {
-        Image(systemName: "globe")
-            .imageScale(.large)
-            .foregroundStyle(.tint)
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 15) {
+                
+            }
+        }
+    }
+    
+    @ViewBuilder func sleepTimeSlider() -> some View {
+        GeometryReader { proxy in
+            let width = proxy.size.width
+            ZStack {
+                 let numbers = [12, 15, 18, 21, 0, 3, 6, 9]
+                
+                ForEach(numbers.indices, id: \.self) {
+                    index in Text("\(numbers[index])")
+                        .foregroundColor(.secondary)
+                        .font(.caption)
+                        .rotationEffect(.init(degrees: Double(index) * -45))
+                        .offset(y: (width - 90) / 2)
+                        .rotationEffect(.init(degrees: Double(index) * 45))
+                }
+                Circle()
+                    
+            }
+        }
     }
 }
 
